d294f11601f4b939473dc0eb90303dc031f7c10b
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2023-01-27 09:26:10 -0800

    Remove splerger.

--- addons/splerger/run_split.gd
+++
@@ -1,9 +0,0 @@
-extends Node3D
-
-const sperlger_const = preload("res://addons/splerger/merge_splerger.gd")
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	sperlger_const.traverse_root_and_merge(self)
-
-
