6bede61208499b2ab46a0995876b61d392a663cc
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2023-02-04 14:09:48 -0800

    Use an guard conditional.

--- addons/vsk_manager/vsk_startup_manager.gd
+++ addons/vsk_manager/vsk_startup_manager.gd
@@ -122,61 +122,62 @@ func parse_commandline_args() -> void:
 		OS.get_cmdline_args()
 	)
 	display_name_override = ""
-	if !Engine.is_editor_hint():
-		# Check if we're running in headless mode
-		if is_headless():
-			VSKGameFlowManager.autoquit = true
+	if Engine.is_editor_hint():
+		return
+	# Check if we're running in headless mode
+	if is_headless():
+		VSKGameFlowManager.autoquit = true
 
-		if commandline_argument_dictionary.has("port"):
-			if commandline_argument_dictionary["port"][0].is_valid_int():
-				port = commandline_argument_dictionary["port"][0].to_int()
+	if commandline_argument_dictionary.has("port"):
+		if commandline_argument_dictionary["port"][0].is_valid_int():
+			port = commandline_argument_dictionary["port"][0].to_int()
 
-		if commandline_argument_dictionary.has("ip"):
-			ip = commandline_argument_dictionary["ip"][0]
-		if commandline_argument_dictionary.has("display_name"):
-			display_name_override = commandline_argument_dictionary["display_name"]
-		if commandline_argument_dictionary.has("use_flat"):
-			VRManager.vr_user_preferences.vr_mode_override = (
-				VRManager.vr_user_preferences.vr_mode_override_enum.VR_MODE_USE_FLAT
-			)
-		if commandline_argument_dictionary.has("use_vr"):
-			VRManager.vr_user_preferences.vr_mode_override = (
-				VRManager.vr_user_preferences.vr_mode_override_enum.VR_MODE_USE_VR
-			)
-		if commandline_argument_dictionary.has("dedicated"):
-			is_dedicated = true
-		if commandline_argument_dictionary.has("public"):
-			is_public = true
-		if commandline_argument_dictionary.has("map"):
-			map = commandline_argument_dictionary["map"][0]
-		if commandline_argument_dictionary.has("game_mode"):
-			game_mode = commandline_argument_dictionary["game_mode"][0]
+	if commandline_argument_dictionary.has("ip"):
+		ip = commandline_argument_dictionary["ip"][0]
+	if commandline_argument_dictionary.has("display_name"):
+		display_name_override = commandline_argument_dictionary["display_name"]
+	if commandline_argument_dictionary.has("use_flat"):
+		VRManager.vr_user_preferences.vr_mode_override = (
+			VRManager.vr_user_preferences.vr_mode_override_enum.VR_MODE_USE_FLAT
+		)
+	if commandline_argument_dictionary.has("use_vr"):
+		VRManager.vr_user_preferences.vr_mode_override = (
+			VRManager.vr_user_preferences.vr_mode_override_enum.VR_MODE_USE_VR
+		)
+	if commandline_argument_dictionary.has("dedicated"):
+		is_dedicated = true
+	if commandline_argument_dictionary.has("public"):
+		is_public = true
+	if commandline_argument_dictionary.has("map"):
+		map = commandline_argument_dictionary["map"][0]
+	if commandline_argument_dictionary.has("game_mode"):
+		game_mode = commandline_argument_dictionary["game_mode"][0]
 
-		if commandline_argument_dictionary.has("max_players"):
-			if commandline_argument_dictionary["max_players"][0].is_valid_int():
-				max_players = commandline_argument_dictionary["max_players"][0].to_int()
-		if commandline_argument_dictionary.has("max_retries"):
-			if commandline_argument_dictionary["max_retries"][0].is_valid_int():
-				max_retries = commandline_argument_dictionary["max_retries"][0].to_int()
+	if commandline_argument_dictionary.has("max_players"):
+		if commandline_argument_dictionary["max_players"][0].is_valid_int():
+			max_players = commandline_argument_dictionary["max_players"][0].to_int()
+	if commandline_argument_dictionary.has("max_retries"):
+		if commandline_argument_dictionary["max_retries"][0].is_valid_int():
+			max_retries = commandline_argument_dictionary["max_retries"][0].to_int()
 
-		if commandline_argument_dictionary.has("server_name"):
-			server_name = commandline_argument_dictionary["server_name"][0]
-		if commandline_argument_dictionary.has("test_audio"):
-			test_audio = commandline_argument_dictionary["test_audio"][0]
-		if commandline_argument_dictionary.has("autoquit"):
-			VSKGameFlowManager.autoquit = true
+	if commandline_argument_dictionary.has("server_name"):
+		server_name = commandline_argument_dictionary["server_name"][0]
+	if commandline_argument_dictionary.has("test_audio"):
+		test_audio = commandline_argument_dictionary["test_audio"][0]
+	if commandline_argument_dictionary.has("autoquit"):
+		VSKGameFlowManager.autoquit = true
 
-		# Network Latency Simulator
-		if commandline_argument_dictionary.has("simulate_network_conditions"):
-			NetworkManager.network_flow_manager.simulate_network_conditions = true
-		if commandline_argument_dictionary.has("min_latency"):
-			NetworkManager.network_flow_manager.min_latency = commandline_argument_dictionary["min_latency"][0]
-		if commandline_argument_dictionary.has("max_latency"):
-			NetworkManager.network_flow_manager.max_latency = commandline_argument_dictionary["max_latency"][0]
-		if commandline_argument_dictionary.has("drop_rate"):
-			NetworkManager.network_flow_manager.drop_rate = commandline_argument_dictionary["drop_rate"][0]
-		if commandline_argument_dictionary.has("dup_rate"):
-			NetworkManager.network_flow_manager.dup_rate = commandline_argument_dictionary["dup_rate"][0]
+	# Network Latency Simulator
+	if commandline_argument_dictionary.has("simulate_network_conditions"):
+		NetworkManager.network_flow_manager.simulate_network_conditions = true
+	if commandline_argument_dictionary.has("min_latency"):
+		NetworkManager.network_flow_manager.min_latency = commandline_argument_dictionary["min_latency"][0]
+	if commandline_argument_dictionary.has("max_latency"):
+		NetworkManager.network_flow_manager.max_latency = commandline_argument_dictionary["max_latency"][0]
+	if commandline_argument_dictionary.has("drop_rate"):
+		NetworkManager.network_flow_manager.drop_rate = commandline_argument_dictionary["drop_rate"][0]
+	if commandline_argument_dictionary.has("dup_rate"):
+		NetworkManager.network_flow_manager.dup_rate = commandline_argument_dictionary["dup_rate"][0]
 
 
 func apply_project_settings() -> void:
@@ -198,10 +199,11 @@ func _ready() -> void:
 
 
 func _init():
-	if !Engine.is_editor_hint():
-		if OS.get_name() == "Server":
-			headless_flag = true
+	if Engine.is_editor_hint():
+		return
+	if OS.get_name() == "Server":
+		headless_flag = true
 
-		parse_commandline_args()
+	parse_commandline_args()
 
-		managers_requiring_preloading = [VSKMenuManager, VSKNetworkManager]
+	managers_requiring_preloading = [VSKMenuManager, VSKNetworkManager]
