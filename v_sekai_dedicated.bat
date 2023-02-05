e6bd3ded951b41bf29d12005db293e8666d1830c
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2023-02-04 16:22:03 -0800

    Restored headless detection to launch maps on the cloud.

--- v_sekai_dedicated.bat
+++ v_sekai_dedicated.bat
@@ -1,1 +1,1 @@
-v-sekai.exe --xr-mode off --headless --dedicated --use_flat --map res://vsk_default/scenes/maps/sky_celebration/sky_celebration.tscn --server_name "V-Sekai Server" --autoquit --max_retries 100
+v-sekai.exe --xr-mode off --headless --dedicated --use_flat --server_name "V-Sekai Server" --autoquit --max_retries 100 --map "uro://bb7d83a8-a92d-4ba4-8e3f-233468c3e9db"
