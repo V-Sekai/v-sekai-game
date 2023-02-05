0d9729c88c97948003efb5f345236178c527cc50
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2023-02-04 16:25:45 -0800

    Use the script.

--- v_sekai_dedicated.bat
+++ v_sekai_dedicated.bat
@@ -1,1 +1,1 @@
-v-sekai.exe --xr-mode off --headless --dedicated --use_flat --server_name "V-Sekai Server" --autoquit --max_retries 100 --map "uro://bb7d83a8-a92d-4ba4-8e3f-233468c3e9db"
+v-sekai.exe --headless --dedicated --server_name "V-Sekai Server" --max_retries 100 --map "uro://bb7d83a8-a92d-4ba4-8e3f-233468c3e9db"
