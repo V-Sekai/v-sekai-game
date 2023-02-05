60b42ade35f202b54b955d7d65881a3fc43100d0
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2023-02-04 16:27:57 -0800

    Don't run openxr headless.

--- v_sekai_dedicated.bat
+++ v_sekai_dedicated.bat
@@ -1,1 +1,1 @@
-v-sekai.exe --headless --dedicated --server_name "V-Sekai Server" --max_retries 100 --map "uro://bb7d83a8-a92d-4ba4-8e3f-233468c3e9db"
+v-sekai.exe --dedicated --server_name "V-Sekai Server" --max_retries 100 --map "uro://bb7d83a8-a92d-4ba4-8e3f-233468c3e9db" --headless --xr-mode off
