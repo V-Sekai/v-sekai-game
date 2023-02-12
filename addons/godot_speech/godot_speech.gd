e6c598ced512d3257487ed33abc9183f114eabca
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2023-02-12 03:21:10 -0800

    Audio monitor is not necessary.

--- addons/godot_speech/godot_speech.gd
+++ addons/godot_speech/godot_speech.gd
@@ -1,14 +1,7 @@
 extends Speech
 
-var audio_stats_timer: Timer = Timer.new()
-
 
 func _ready() -> void:
-	add_child(audio_stats_timer)
-	audio_stats_timer.owner = owner
-	audio_stats_timer.wait_time = 0.5
-	audio_stats_timer.timeout.connect(Callable(self, "print_audio_stats"))
-	audio_stats_timer.start()
 	if !Engine.is_editor_hint():
 		set_name("GodotSpeech")
 		assert(NetworkManager.peer_unregistered.connect(remove_player_audio) == OK)
