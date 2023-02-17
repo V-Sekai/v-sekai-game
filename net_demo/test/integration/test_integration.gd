85afbfd062f59813e86bb500856e48c8256dced9
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2023-02-17 06:32:50 -0800

    Remove placeholder tests.

--- net_demo/test/integration/test_integration.gd
+++
@@ -1,4 +0,0 @@
-extends GutTest
-
-func test_assert_eq_integration_string_equal():
-	assert_eq('Integration', 'Integration', "Should pass")
--- net_demo/test/unit/test_unit.gd
+++
@@ -1,4 +0,0 @@
-extends GutTest
-
-func test_assert_eq_test_string_equal():
-	assert_eq('Test', 'Test', "Should pass")
