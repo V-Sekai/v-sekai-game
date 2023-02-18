5b5729c90d6fe947314421340bc09df257b8cef0
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2023-02-18 09:22:08 -0800

    Avoid errors on the build system.

--- net_demo/core/entity/physics_object_multiplayer_synchronizer.gd
+++ net_demo/core/entity/physics_object_multiplayer_synchronizer.gd
@@ -1,15 +1,16 @@
 extends MultiplayerSynchronizer
 
+
 # Any peer can call this function
 @rpc("any_peer", "call_local") func claim_authority() -> void:
 	var sender_id: int = multiplayer.get_remote_sender_id()
-	if GameManager.is_session_authority(multiplayer.get_unique_id()):
-		MultiplayerPhysicsOwnershipTracker.request_authority(self, sender_id)
+	if $/root/GameManager.is_session_authority(multiplayer.get_unique_id()):
+		$/root/MultiplayerPhysicsOwnershipTracker.request_authority(self, sender_id)
 
 
 @rpc("any_peer", "call_local") func assign_authority(p_peer_id: int) -> void:
 	var sender_id: int = multiplayer.get_remote_sender_id()
-	if GameManager.is_session_authority(sender_id):
+	if $/root/GameManager.is_session_authority(sender_id):
 		var physics_body: PhysicsBody3D = get_node(root_path)
 		if physics_body:
 			physics_body.set_multiplayer_authority(p_peer_id)
@@ -18,7 +19,7 @@ extends MultiplayerSynchronizer
 
 func player_spawned(p_peer_id: int) -> void:
 	if multiplayer.get_unique_id() == 1:
-		if is_multiplayer_authority() and GameManager.is_dedicated_server:
+		if is_multiplayer_authority() and $/root/GameManager.is_dedicated_server:
 			assert(rpc_id(p_peer_id, "assign_authority", p_peer_id) == OK)
 			var physics_body: PhysicsBody3D = get_node(root_path)
 			if physics_body:
@@ -28,19 +29,19 @@ func player_spawned(p_peer_id: int) -> void:
 
 
 func player_unspawned(p_peer_id: int) -> void:
-	if GameManager.player_list.find(p_peer_id) == -1:
+	if $/root/GameManager.player_list.find(p_peer_id) == -1:
 		var physics_body: PhysicsBody3D = get_node(root_path)
 		if physics_body:
 			if physics_body.get_multiplayer_authority() == p_peer_id:
-				physics_body.set_multiplayer_authority(GameManager.get_session_authority())
+				physics_body.set_multiplayer_authority($/root/GameManager.get_session_authority())
 				physics_body.pending_authority_request = false
 
 
 func _ready():
-	assert(GameManager.player_spawned.connect(player_spawned) == OK)
-	assert(GameManager.player_unspawned.connect(player_unspawned) == OK)
+	assert($/root/GameManager.player_spawned.connect(player_spawned) == OK)
+	assert($/root/GameManager.player_unspawned.connect(player_unspawned) == OK)
 
 	if get_multiplayer_authority() == 1:
 		var physics_body: PhysicsBody3D = get_node(root_path)
 		if physics_body:
-			physics_body.set_multiplayer_authority(GameManager.get_session_authority())
+			physics_body.set_multiplayer_authority($/root/GameManager.get_session_authority())
