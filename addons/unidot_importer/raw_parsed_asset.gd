54c5975b021ff47c2081f9cdf69135555f4c63f9
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2023-04-28 01:18:12 -0700

    Remove unidot_importer

--- addons/unidot_importer/raw_parsed_asset.gd
+++
@@ -1,6 +0,0 @@
-extends Resource
-
-@export var path: String = ""
-@export var guid: String = ""
-@export var objects: Dictionary = {}
-@export var meta: Resource = null
--- addons/unidot_importer/static_preload.gd
+++
@@ -1,5 +0,0 @@
-extends Resource
-
-@export var storage: Resource = null
-@export var editor_interface_instance_id: int = 0
-@export var os_instance_id: int = 0
