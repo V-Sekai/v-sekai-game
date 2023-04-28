34b201c2f3d1867ff259b48a70981483933a1726
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2023-04-28 01:17:18 -0700

    git subrepo clone --branch=main https://github.com/V-Sekai/vsk_vrm_avatar_tool addons/vsk_vrm_avatar_tool
    subrepo:
      subdir:   "addons/vsk_vrm_avatar_tool"
      merged:   "091df4f"
    upstream:
      origin:   "https://github.com/V-Sekai/vsk_vrm_avatar_tool"
      branch:   "main"
      commit:   "091df4f"
    git-subrepo:
      version:  "0.4.6"
      origin:   "???"
      commit:   "???"

---
+++ addons/vsk_vrm_avatar_tool/vsk_vrm_callback.gd
@@ -0,0 +1,35 @@
+@tool
+extends RefCounted
+
+enum {
+	VRM_OK,
+	VRM_FAILED,
+	VRM_INVALID_MENU_OPTION,
+	VRM_COULD_NOT_SAVE,
+	VRM_COULD_NOT_PACK,
+	VRM_INVALID_NODE,
+	VRM_NO_EDITOR_PLUGIN
+}
+
+
+static func get_error_string(p_err: int) -> String:
+	var error_str: String = "Unknown error!"
+	match p_err:
+		VRM_FAILED:
+			error_str = "Generic VRM error! (complain to Saracen)"
+		VRM_INVALID_MENU_OPTION:
+			error_str = "Invalid menu option"
+		VRM_COULD_NOT_SAVE:
+			error_str = "Could not be saved"
+		VRM_COULD_NOT_PACK:
+			error_str = "Could not be packed"
+		VRM_INVALID_NODE:
+			error_str = "Invalid node"
+		VRM_NO_EDITOR_PLUGIN:
+			error_str = "No editor plugin found"
+
+	return error_str
+
+
+static func generic_error_check(p_root: Node3D) -> int:
+	return VRM_OK
