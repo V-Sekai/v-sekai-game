474b33b1d2e3e507668460ab998be9426d897eb1
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2023-04-29 20:50:55 -0700

    git subrepo clone --branch=main https://github.com/V-Sekai/gd_util addons/gd_util
    subrepo:
      subdir:   "addons/gd_util"
      merged:   "f423589"
    upstream:
      origin:   "https://github.com/V-Sekai/gd_util"
      branch:   "main"
      commit:   "f423589"
    git-subrepo:
      version:  "0.4.6"
      origin:   "https://github.com/ingydotnet/git-subrepo"
      commit:   "110b9eb"

---
+++ addons/gd_util/sprite_gizmo.gd
@@ -0,0 +1,34 @@
+@tool
+extends EditorNode3DGizmo
+
+const immediate_shape_util_const = preload("immediate_shape_util.gd")
+
+var plugin = null
+var texture = Texture.new()
+
+
+func get_handle_name(p_idx: int) -> String:
+	return ""
+
+
+func get_handle_value(p_idx: int) -> int:
+	return 0
+
+
+func set_handle(p_idx, p_spatial, p_point):
+	pass
+
+
+func commit_handle(index, restore, cancel = false):
+	pass
+
+
+func redraw():
+	clear()
+	var icon_material: Material = immediate_shape_util_const.create_icon_material(texture, Color())
+	add_unscaled_billboard(icon_material, 0.05)
+
+
+func _init(p_plugin, p_texture):
+	texture = p_texture
+	plugin = p_plugin
