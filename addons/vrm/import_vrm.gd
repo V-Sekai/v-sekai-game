6b6a32065205161d2ab6e9b37e563731e2e82650
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2023-04-29 20:51:32 -0700

    git subrepo clone --branch=only-addon https://github.com/V-Sekai/godot-vrm addons/vrm
    subrepo:
      subdir:   "addons/vrm"
      merged:   "3cbcc07"
    upstream:
      origin:   "https://github.com/V-Sekai/godot-vrm"
      branch:   "only-addon"
      commit:   "3cbcc07"
    git-subrepo:
      version:  "0.4.6"
      origin:   "https://github.com/ingydotnet/git-subrepo"
      commit:   "110b9eb"

---
+++ addons/vrm/import_vrm.gd
@@ -0,0 +1,38 @@
+@tool
+extends EditorSceneFormatImporter
+
+const gltf_document_extension_class = preload("./vrm_extension.gd")
+
+
+func _get_importer_name() -> String:
+	return "Godot-VRM"
+
+
+func _get_recognized_extensions() -> Array:
+	return ["vrm"]
+
+
+func _get_extensions() -> PackedStringArray:
+	var exts: PackedStringArray
+	exts.push_back("vrm")
+	return exts
+
+
+func _get_import_flags() -> int:
+	return IMPORT_SCENE
+
+
+func _import_scene(path: String, flags: int, options: Dictionary) -> Object:
+	var gltf: GLTFDocument = GLTFDocument.new()
+	flags |= EditorSceneFormatImporter.IMPORT_USE_NAMED_SKIN_BINDS
+	var vrm_extension: GLTFDocumentExtension = gltf_document_extension_class.new()
+	gltf.register_gltf_document_extension(vrm_extension, true)
+	var state: GLTFState = GLTFState.new()
+	state.handle_binary_image = GLTFState.HANDLE_BINARY_EMBED_AS_BASISU
+	var err = gltf.append_from_file(path, state, flags)
+	if err != OK:
+		gltf.unregister_gltf_document_extension(vrm_extension)
+		return null
+	var generated_scene = gltf.generate_scene(state)
+	gltf.unregister_gltf_document_extension(vrm_extension)
+	return generated_scene
