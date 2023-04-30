180935a01e23390d8353b9ba4ec59f8912b0070c
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2023-04-29 20:51:38 -0700

    git subrepo clone https://github.com/V-Sekai/unidot_importer addons/unidot_importer
    subrepo:
      subdir:   "addons/unidot_importer"
      merged:   "2337cff"
    upstream:
      origin:   "https://github.com/V-Sekai/unidot_importer"
      branch:   "main"
      commit:   "2337cff"
    git-subrepo:
      version:  "0.4.6"
      origin:   "https://github.com/ingydotnet/git-subrepo"
      commit:   "110b9eb"

---
+++ addons/unidot_importer/plugin.gd
@@ -0,0 +1,53 @@
+@tool
+extends EditorPlugin
+
+const tarfile = preload("./tarfile.gd")
+
+const package_import_dialog_class: GDScript = preload("./package_import_dialog.gd")
+
+var package_import_dialog: RefCounted = null
+
+
+func recursive_print(node: Node, indent: String = ""):
+	var fnstr = "" if str(node.filename) == "" else (" (" + str(node.filename) + ")")
+	print(indent + str(node.name) + ": owner=" + str(node.owner.name if node.owner != null else "") + fnstr)
+	#print(indent + str(node.name) + str(node) + ": owner=" + str(node.owner.name if node.owner != null else "") + str(node.owner) + fnstr)
+	var new_indent: String = indent + "  "
+	for c in node.get_children():
+		recursive_print(c, new_indent)
+
+
+func queue_test():
+	var queue_lib: GDScript = load("./queue_lib.gd")
+	var q = queue_lib.new()
+	q.run_test()
+
+
+func show_reimport():
+	package_import_dialog = package_import_dialog_class.new()
+	package_import_dialog.show_reimport()
+
+
+func show_importer():
+	package_import_dialog = package_import_dialog_class.new()
+	package_import_dialog.show_importer()
+
+
+func recursive_print_scene():
+	recursive_print(get_tree().edited_scene_root)
+
+
+func _enter_tree():
+	print("run enter tree")
+	add_tool_menu_item("Import Unity Package...", self.show_importer)
+	add_tool_menu_item("Reimport large unity package...", self.show_reimport)
+	add_tool_menu_item("Queue Test...", self.queue_test)
+	add_tool_menu_item("Print scene nodes with owner...", self.recursive_print_scene)
+
+
+func _exit_tree():
+	print("run exit tree")
+	remove_tool_menu_item("Print scene nodes with owner...")
+	remove_tool_menu_item("Import Unity Package...")
+	remove_tool_menu_item("Reimport large unity package...")
+	remove_tool_menu_item("Queue Test...")
