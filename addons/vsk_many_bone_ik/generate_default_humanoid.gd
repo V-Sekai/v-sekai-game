a4a5f3d98069e0575002c6ba1af6bca2816d67a8
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2023-06-04 17:09:45 -0700

    Fix the elbows and improve the constraints.

--- addons/vsk_many_bone_ik/generate_default_humanoid.gd
+++ addons/vsk_many_bone_ik/generate_default_humanoid.gd
@@ -53,13 +53,13 @@ var config: Dictionary = {
 	},
 	"Head": {
 		"swing_rotation_center_radius": [
-			{"center": Vector3(0, 1, 0), "radius": 0.0872664626}  # 5 degrees
+			{"center": Vector3(0, 1, 0), "radius": 0.0436332313}  # 2.5 degrees
 		],
 		"twist_rotation_range": {
 			"from": 0,  # 0 degrees
-			"range": 0.0436332313  # 2.5 degrees
+			"range": 0.02181661565  # 1.25 degrees
 		}
-	},
+	},	
 	"LeftEye": {
 		"swing_rotation_center_radius": [
 			{"center": Vector3(1, 0, 0), "radius": 0.0436332313},  # 2.5 degrees
@@ -73,7 +73,7 @@ var config: Dictionary = {
 	"LeftShoulder": {
 		"swing_rotation_center_radius": [
 			{"center": Vector3(1, 0.5, 0), "radius": 0.3490658504},  # 20 degrees
-			{"center": Vector3(0, 1, 0.5), "radius": 0.2617993878}   # 15 degrees
+			{"center": Vector3(0, 1, 0.5), "radius": 0.2617993878},   # 15 degrees
 		],
 		"twist_rotation_range": {
 			"from": 0,  # 0 degrees
@@ -82,19 +82,18 @@ var config: Dictionary = {
 	},
 	"LeftUpperArm": {
 		"swing_rotation_center_radius": [
-			{"center": Vector3(1, 0, 0.25), "radius": 0.872664626},  # 50 degrees
-			{"center": Vector3(0, -0.25, 1), "radius": 0.872664626}   # 50 degrees
+			{"center": Vector3(1, 0, 0.25), "radius": 0.523598776},  # 30 degrees
+			{"center": Vector3(0, -0.25, 1), "radius": 0.523598776}   # 30 degrees
 		],
 		"twist_rotation_range": {
 			"from": 0,  # 0 degrees
-			"range": 0.2617993878  # 15 degrees
+			"range": 0.3490658504  # 20 degrees
 		}
 	},
 	"LeftLowerArm": {
 		"swing_rotation_center_radius": [
-			{"center": Vector3(1, 0, 0.5), "radius": 0.698131701},  # 40 degrees - Top segment
-			{"center": Vector3(0, 1, 0.5), "radius": 0.698131701},  # 40 degrees - Middle segment
-			{"center": Vector3(0, 0, 1), "radius": 0.698131701}     # 40 degrees - Bottom segment
+			{"center": Vector3(1, 0, 0), "radius": 0.3490658504},  # 20 degrees - Top segment
+			{"center": Vector3(0, 1, 0), "radius": 0.3490658504}   # 20 degrees - Middle segment (flipped front to back)
 		],
 		"twist_rotation_range": {
 			"from": 0,  # 0 degrees
@@ -103,7 +102,7 @@ var config: Dictionary = {
 	},
 	"LeftHand": {
 		"swing_rotation_center_radius": [
-			{"center": Vector3(0, 1, 0), "radius": 1.5707963268},  # 90 degrees
+			{"center": Vector3(0, 1, 0), "radius": 0.7853981634},  # 45 degrees
 		],
 		"twist_rotation_range": {
 			"from": 0,  # 0 degrees
@@ -162,7 +161,7 @@ var config: Dictionary = {
 	"RightShoulder": {
 		"swing_rotation_center_radius": [
 			{"center": Vector3(-1, 0.5, 0), "radius": 0.3490658504},  # 20 degrees
-			{"center": Vector3(0, 1, -0.5), "radius": 0.2617993878}   # 15 degrees
+			{"center": Vector3(0, 1, -0.5), "radius": 0.2617993878},   # 15 degrees
 		],
 		"twist_rotation_range": {
 			"from": 0,  # 0 degrees
@@ -171,19 +170,18 @@ var config: Dictionary = {
 	},
 	"RightUpperArm": {
 		"swing_rotation_center_radius": [
-			{"center": Vector3(-0.25, 0, 1), "radius": 0.872664626},  # 50 degrees
-			{"center": Vector3(-1, 0.25, 0), "radius": 0.872664626}   # 50 degrees
+			{"center": Vector3(-0.25, 0, 1), "radius": 0.523598776},  # 30 degrees
+			{"center": Vector3(-1, 0.25, 0), "radius": 0.523598776}   # 30 degrees
 		],
 		"twist_rotation_range": {
 			"from": 0,  # 0 degrees
-			"range": 0.2617993878  # 15 degrees
+			"range": 0.3490658504  # 20 degrees
 		}
 	},
 	"RightLowerArm": {
 		"swing_rotation_center_radius": [
-			{"center": Vector3(1, 0, 0.5), "radius": 0.698131701},  # 40 degrees - Top segment
-			{"center": Vector3(0, 1, 0.5), "radius": 0.698131701},  # 40 degrees - Middle segment
-			{"center": Vector3(0, 0, 1), "radius": 0.698131701}     # 40 degrees - Bottom segment
+			{"center": Vector3(-1, 0, 0), "radius": 0.3490658504},  # 20 degrees - Top segment
+			{"center": Vector3(0, 1, 0), "radius": 0.3490658504},   # 20 degrees - Middle segment
 		],
 		"twist_rotation_range": {
 			"from": 0,  # 0 degrees
@@ -192,7 +190,7 @@ var config: Dictionary = {
 	},
 	"RightHand": {
 		"swing_rotation_center_radius": [
-			{"center": Vector3(0, 1, 0), "radius": 1.5707963268},  # 90 degrees
+			{"center": Vector3(0, 1, 0), "radius": 0.7853981634},  # 45 degrees
 		],
 		"twist_rotation_range": {
 			"from": 0,  # 0 degrees
