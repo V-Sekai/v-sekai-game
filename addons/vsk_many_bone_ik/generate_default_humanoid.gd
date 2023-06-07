8307c379461445b3cbaddabb76fe7347156b869c
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2023-06-07 04:52:40 -0700

    Update to the new api.

--- addons/vsk_many_bone_ik/generate_default_humanoid.gd
+++ addons/vsk_many_bone_ik/generate_default_humanoid.gd
@@ -10,19 +10,19 @@ func calculate_constraint_progress(skeleton: Skeleton3D, bone_name: String) -> f
 		var rest_pose_basis = skeleton.get_bone_rest(bone_index).basis
 
 		if bone_config.has("twist_rotation_range"):
-			var twist_from = bone_config["twist_rotation_range"]["from"]
-			var twist_range = bone_config["twist_rotation_range"]["range"]
+			var twist_from = bone_config["twist_rotation_range"][0]
+			var twist_to = bone_config["twist_rotation_range"][1]
 			var twist_diff = abs(current_pose_basis.get_euler().y - rest_pose_basis.get_euler().y)
-			progress = max(progress, (twist_diff - twist_from) / twist_range)
+			progress = max(progress, (twist_diff - twist_from) / (twist_to - twist_from))
 
-		if bone_config.has("swing_rotation_center_radius"):
-			for cone in bone_config["swing_rotation_center_radius"]:
-				var center = cone["center"]
-				var radius = cone["radius"]
+		if bone_config.has("swing_spherical_coords"):
+			for cone in bone_config["swing_spherical_coords"]:
+				var center = spherical_to_cartesian(Vector2(cone[0], cone[1]))
+				var radius = cone[2]
 				var swing_diff_quat = current_pose_basis.get_rotation_quaternion().inverse() * rest_pose_basis.get_rotation_quaternion()
 				var swing_diff_angle = swing_diff_quat.get_angle()
 				progress = max(progress, (swing_diff_angle - center.length()) / radius)
-	
+
 	return progress
 
 
@@ -39,29 +39,26 @@ func print_bone_report():
 				var progress = calculate_constraint_progress(skeleton, bone_name)
 				var action = ""
 
-				if progress < 0.25:
+				var current_pose = skeleton.get_bone_global_pose_no_override(bone_index)
+				var rest_pose = skeleton.get_bone_rest(bone_index)
+				var position_distance = current_pose.origin.distance_to(rest_pose.origin) * 1000  # Convert to mm
+				var rotation_difference = (current_pose.basis.get_euler() - rest_pose.basis.get_euler()) * 180 / PI  # Convert to degrees
+
+				if progress < 0.25 and position_distance < 1000:
 					action = "Good"
-				elif progress < 0.5:
+				elif progress < 0.5 or (position_distance >= 1000 and position_distance < 2000):
 					action = "Slightly out of range"
-				elif progress < 0.75:
+				elif progress < 0.75 or (position_distance >= 2000 and position_distance < 3000):
 					action = "Out of range"
 				else:
 					action = "Significantly out of range"
 
-				var current_rotation_angles = skeleton.get_bone_global_pose_no_override(bone_index).basis.get_euler()
-				var parent_bone_distance = 0.0
-				if skeleton.get_bone_parent(bone_index) != -1:
-					var parent_bone_pose = skeleton.get_bone_global_pose_no_override(skeleton.get_bone_parent(bone_index))
-					parent_bone_distance = parent_bone_pose.origin.distance_to(skeleton.get_bone_global_pose_no_override(bone_index).origin)
+				print("Bone: %s | Progress: %.2f | Suggested Action: %s | Rotation Difference: %s° | Distance from Rest Pose: %.2fmm" % [bone_name, progress, action, rotation_difference, position_distance])
 
-				print("Bone: %s | Progress: %.2f | Suggested Action: %s | Rotation Angles: %s | Distance from Parent Bone: %.2f" % [bone_name, progress, action, current_rotation_angles, parent_bone_distance])
-			else:
-				print("Bone: %s | Not in config" % bone_name)
-	
-func create_symmetric_entry(name, swing_rotation_center_radius, twist_rotation_range_from, twist_rotation_range_range):
+func create_symmetric_entry(name, swing_spherical_coords, twist_rotation_range_from, twist_rotation_range_range):
 	var entry = {
 		name: {
-			"swing_rotation_center_radius": swing_rotation_center_radius,
+			"swing_spherical_coords": swing_spherical_coords,
 			"twist_rotation_range": {
 				"from": deg_to_rad(twist_rotation_range_from),
 				"range": deg_to_rad(twist_rotation_range_range)
@@ -70,19 +67,19 @@ func create_symmetric_entry(name, swing_rotation_center_radius, twist_rotation_r
 	}
 	var right_name = name.replace("Left", "Right")
 	entry[right_name] = {
-		"swing_rotation_center_radius": [],
+		"swing_spherical_coords": [],
 		"twist_rotation_range": {
 			"from": deg_to_rad(twist_rotation_range_from),
 			"range": deg_to_rad(twist_rotation_range_range)
 		}
 	}
 
-	for center_radius in swing_rotation_center_radius:
-		var center = center_radius.get("center", Vector3(0, 1, 0))
+	for center_radius in swing_spherical_coords:
+		var center = center_radius.get("center", Vector2(0, 1))
 		var radius = center_radius["radius"]
-		var new_center = Vector3(-center.x, center.y, center.z)
+		var new_center = Vector2(-center.x, center.y)
 
-		entry[right_name]["swing_rotation_center_radius"].append({"center": new_center, "radius": radius})
+		entry[right_name]["swing_spherical_coords"].append({"center": new_center, "radius": radius})
 
 	return entry
 
@@ -93,84 +90,61 @@ func spherical_to_cartesian(theta_phi: Vector2) -> Vector3:
 	var y = sin(phi) * sin(theta)
 	var z = cos(phi)
 	return Vector3(x, y, z)
-
+	
 func generate_config():
-	var symmetric_entries = [
-		["LeftEye", [{"center": spherical_to_cartesian(Vector2(deg_to_rad(90), deg_to_rad(2.5))), "radius": deg_to_rad(2.5)}, {"center": spherical_to_cartesian(Vector2(deg_to_rad(0), deg_to_rad(2.5))), "radius": deg_to_rad(2.5)}], 0, 2.5],
-		["LeftShoulder", [{"center": spherical_to_cartesian(Vector2(deg_to_rad(90), deg_to_rad(4))), "radius": deg_to_rad(4)}, {"center": spherical_to_cartesian(Vector2(deg_to_rad(0), deg_to_rad(4))), "radius": deg_to_rad(4)}], 0, 4],
-		["LeftUpperArm", [{"center": spherical_to_cartesian(Vector2(deg_to_rad(90), deg_to_rad(20))), "radius": deg_to_rad(20)}, {"center": spherical_to_cartesian(Vector2(deg_to_rad(180), deg_to_rad(20))), "radius": deg_to_rad(20)}], 0, 12],
-		["LeftLowerArm", [{"center": spherical_to_cartesian(Vector2(deg_to_rad(0), deg_to_rad(25))), "radius": deg_to_rad(25)}, {"center": spherical_to_cartesian(Vector2(deg_to_rad(270), deg_to_rad(25))), "radius": deg_to_rad(25)}, {"center": spherical_to_cartesian(Vector2(deg_to_rad(180), deg_to_rad(25))), "radius": deg_to_rad(25)}], 45, 8],
-		["LeftHand", [{"center": spherical_to_cartesian(Vector2(deg_to_rad(0), deg_to_rad(40))), "radius": deg_to_rad(40)}], 0, 80],
-		["LeftUpperLeg", [
-			{"center": spherical_to_cartesian(Vector2(deg_to_rad(90), deg_to_rad(25))), "radius": deg_to_rad(25)},
-			{"center": spherical_to_cartesian(Vector2(deg_to_rad(180), deg_to_rad(25))), "radius": deg_to_rad(25)}], 85, 2],
-		["LeftLowerLeg", [{"center": spherical_to_cartesian(Vector2(deg_to_rad(0), deg_to_rad(35))), "radius": deg_to_rad(35)}, {"center": spherical_to_cartesian(Vector2(deg_to_rad(90), deg_to_rad(35))), "radius": deg_to_rad(35)}, {"center": spherical_to_cartesian(Vector2(deg_to_rad(180), deg_to_rad(35))), "radius": deg_to_rad(35)}], 45, 8],
-		["LeftFoot", [{"center": spherical_to_cartesian(Vector2(deg_to_rad(180), deg_to_rad(40))), "radius": deg_to_rad(40)}, {"center": spherical_to_cartesian(Vector2(deg_to_rad(270), deg_to_rad(40))), "radius": deg_to_rad(40)}], 0, 4],
-		["LeftToes", [{"center": spherical_to_cartesian(Vector2(deg_to_rad(180), deg_to_rad(15))), "radius": deg_to_rad(15)}], 0, 4],
-	]
 	var config: Dictionary = {
-			"Hips": {
-				"swing_rotation_center_radius": [
-					{"center": Vector3.DOWN, "radius": deg_to_rad(10)}
-				],
-				"twist_rotation_range": {
-					"from": deg_to_rad(90) + deg_to_rad(180),
-					"range": deg_to_rad(5)
-				}
-			},
-			"Spine": {
-				"swing_rotation_center_radius": [
-					{"center": Vector3.UP, "radius": deg_to_rad(15)}
-				],
-				"twist_rotation_range": {
-					"from": deg_to_rad(0),
-					"range": deg_to_rad(2.5)
-				}
-			},
-			"Chest": {
-				"swing_rotation_center_radius": [
-					{"center": Vector3.UP, "radius": deg_to_rad(10)}
-				],
-				"twist_rotation_range": {
-					"from": deg_to_rad(0),
-					"range": deg_to_rad(15)
-				}
-			},
-			"UpperChest": {
-				"swing_rotation_center_radius": [
-					{"center": Vector3.UP, "radius": deg_to_rad(5)}
-				],
-				"twist_rotation_range": {
-					"from": deg_to_rad(0),
-					"range": deg_to_rad(10)
-				}
-			},
-			"Neck": {
-				"swing_rotation_center_radius": [
-					{"center": Vector3.UP, "radius": deg_to_rad(22.5)}
-				],
-				"twist_rotation_range": {
-					"from": deg_to_rad(0),
-					"range": deg_to_rad(2.5)
-				}
-			},
-			"Head": {
-				"swing_rotation_center_radius": [
-					{"center": Vector3.UP, "radius": deg_to_rad(2.5)}
-				],
-				"twist_rotation_range": {
-					"from": deg_to_rad(0),
-					"range": deg_to_rad(1.25)
-				}
-			},
+		"LeftUpperLeg": {
+			"swing_spherical_coords": [
+				[PI / 2, -PI / 2, deg_to_rad(20)],
+				[PI / 3, -2 * PI / 3, deg_to_rad(20)],
+				[2 * PI / 3, -PI / 3, deg_to_rad(20)]
+			],
+			"twist_rotation_range": [deg_to_rad(-3), deg_to_rad(3)]
+		},
+		"RightUpperLeg": {
+			"swing_spherical_coords": [
+				[PI / 2, -PI / 2, deg_to_rad(20)],
+				[PI / 3, -2 * PI / 3, deg_to_rad(20)],
+				[2 * PI / 3, -PI / 3, deg_to_rad(20)]
+			],
+			"twist_rotation_range": [deg_to_rad(-3), deg_to_rad(3)]
+		},
+		"Hips": {
+			"swing_spherical_coords": [
+				[PI / 6, -(5 * PI / 6), deg_to_rad(3)],
+				[PI / 2, -(PI / 2), deg_to_rad(3)],
+				[5 * PI / 6, -(PI / 6), deg_to_rad(3)]
+			],
+			"twist_rotation_range": [deg_to_rad(270), deg_to_rad(8)]
+		},
+		"Head": {
+			"swing_spherical_coords": [[PI / 2, PI / 2, deg_to_rad(15)]],
+			"twist_rotation_range": [deg_to_rad(-3), deg_to_rad(8)]
+		},
+		"Neck": {
+			"swing_spherical_coords": [[PI / 2, PI / 2, deg_to_rad(10)]],
+			"twist_rotation_range": [deg_to_rad(-3), deg_to_rad(8)]
+		},
+		"UpperChest": {
+			"swing_spherical_coords": [[PI / 2, PI / 2, deg_to_rad(10)]],
+			"twist_rotation_range": [deg_to_rad(-3), deg_to_rad(8)]
+		},
+		"Chest": {
+			"swing_spherical_coords": [[PI / 2, PI / 2, deg_to_rad(10)]],
+			"twist_rotation_range": [deg_to_rad(-3), deg_to_rad(8)]
+		},
+		"Spine": {
+			"swing_spherical_coords": [[PI / 2, PI / 2, deg_to_rad(7)]],
+			"twist_rotation_range": [deg_to_rad(-1), deg_to_rad(4)]
+		},
+		"Root": {
+			"swing_spherical_coords": [[PI / 2, PI / 2, deg_to_rad(7)]],
+			"twist_rotation_range": [deg_to_rad(-1), deg_to_rad(4)]
 		}
-		
-	for entry in symmetric_entries:
-		var new_entry = create_symmetric_entry(entry[0], entry[1], entry[2], entry[3])
-		for key in new_entry.keys():
-			config[key] = new_entry[key]
-
+	}
 	return config
+	
+
 
 @export 
 var config: Dictionary = generate_config()
@@ -195,6 +169,7 @@ func _run():
 	var humanoid_profile: SkeletonProfileHumanoid = SkeletonProfileHumanoid.new()
 	var humanoid_bones: PackedStringArray = []
 	var targets: Dictionary = {
+		"Root": "ManyBoneIK3D",
 		"Hips": "ManyBoneIK3D",
 		"Spine": "ManyBoneIK3D",
 		"Chest": "ManyBoneIK3D",
@@ -223,22 +198,21 @@ func _run():
 
 	for bone_i in skeleton.get_bone_count():
 		var bone_name = skeleton.get_bone_name(bone_i)
-
+	
 		if config.has(bone_name):
 			var bone_config = config[bone_name]
 			if bone_config.has("twist_rotation_range"):
-				var twist: Vector2 = Vector2(bone_config["twist_rotation_range"]["from"], bone_config["twist_rotation_range"]["range"])
+				var twist: Vector2 = Vector2(deg_to_rad(bone_config["twist_rotation_range"][0]), deg_to_rad(bone_config["twist_rotation_range"][1]))
 				new_ik.set_kusudama_twist(bone_i, twist)
-
-			if bone_config.has("swing_rotation_center_radius"):
-				var cones: Array = bone_config["swing_rotation_center_radius"]
+	
+			if bone_config.has("swing_spherical_coords"):
+				var cones: Array = bone_config["swing_spherical_coords"]
 				new_ik.set_kusudama_limit_cone_count(bone_i, cones.size())
 				for cone_i in range(cones.size()):
-					var cone: Dictionary = cones[cone_i]
-					if cone.keys().has("center"):
-						new_ik.set_kusudama_limit_cone_center(bone_i, cone_i, cone["center"])
-					if cone.keys().has("radius"):
-						new_ik.set_kusudama_limit_cone_radius(bone_i, cone_i, cone["radius"])
+					var cone: Array = cones[cone_i]
+					var center: Vector2 = Vector2(cone[0], cone[1])
+					new_ik.set_kusudama_limit_cone_center(bone_i, cone_i, spherical_to_cartesian(center))
+					new_ik.set_kusudama_limit_cone_radius(bone_i, cone_i, cone[2])
 
 	var keys = targets.keys()
 	for target_i in keys.size():
