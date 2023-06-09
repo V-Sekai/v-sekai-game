daba9dd398d503d3d2a1068cc22947db17b79830
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2023-06-08 21:34:16 -0700

    Remove unused fetch item.

--- vsk_utility/fetch.sh
+++ vsk_utility/fetch.sh
@@ -3,7 +3,6 @@
 SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
 git clone https://github.com/V-Sekai/godot.git $SCRIPTPATH/../../vsk_godot -b groups-4.x
-git clone https://github.com/V-Sekai/casync-v-sekai-game.git $SCRIPTPATH/../../vsk_casync
 git clone https://github.com/V-Sekai/godot-vsekai-merge.git $SCRIPTPATH/../../vsk_merge
 git clone https://github.com/V-Sekai/groups-gocd-pipelines.git $SCRIPTPATH/../../vsk_cicd
 cp -rf $SCRIPTPATH/vscode $SCRIPTPATH/../../.vscode
