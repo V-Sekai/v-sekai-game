b7435c33208514f5cf440358379640ad759a4a34
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2023-07-14 12:12:33 -0700

    Misc fixes.
    Fix set_name method call in godot_speech.gd
    The commit fixes a bug in the code where the set_name method was not being called correctly. The fix uses the call_deferred function to ensure proper execution.
    Refactor HTTPState release function to handle null http_pool
    
    The release function in the HTTPState class has been refactored to handle cases where the http_pool is null. This change ensures that the function does not throw an error when called with a null http_pool.
    Update project.godot and launch.json
    
    - Update the Godot version to 4.1 with double precision
    - Adjust GDScript warnings settings
    - Change rendering method to "gl_compatibility"
    - Modify launch.json to build the editor for V-Sekai Game
    Update VR origin and viewport in vr_origin.gd
    - Set desktop_viewport.owner to owner
    - Set desktop_camera.owner to owner
    
    Update VR origin scene in vr_origin.tscn
    - Add XRSimulator node as a child of ARVROrigin
    
    Update spatial_game_viewport_manager.gd
    - Update _update_viewport function to set viewport size based on XR status
    
    Add XRSimulator script file
    Revert "Update VR origin and viewport in vr_origin.gd"
    
    This reverts commit a56c422b842d8453f1af62e3b90dd81856dcc16b.
    
    Save work.

--- addons/godot_speech/godot_speech.gd
+++ addons/godot_speech/godot_speech.gd
@@ -8,7 +8,7 @@ extends Speech
 
 func _ready() -> void:
 	if !Engine.is_editor_hint():
-		set_name("GodotSpeech")
+		set_name.call_deferred("GodotSpeech")
 		var result = NetworkManager.peer_unregistered.connect(remove_player_audio)
 		if result != OK:
 			printerr("Failed to connect signal 'peer_unregistered' to 'remove_player_audio'.")
