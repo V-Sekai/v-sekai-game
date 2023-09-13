8c3465448143d507b224e0b1c4f9512871a5db6b
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2023-09-12 18:01:19 -0700

    Remove unused math utils.

--- addons/math_util/math_funcs.gd
+++ addons/math_util/math_funcs.gd
@@ -6,45 +6,6 @@
 @tool
 extends Node
 
-
-static func smooth_damp_scaler(current : float, target : float, current_velocity : float, smooth_time : float, max_speed : float, delta : float) -> Dictionary:
-	smooth_time = max(0.0001, smooth_time)
-	var value_a : float = 2.0 / smooth_time
-	var value_b : float = value_a * delta
-	var value_c : float = 1.0 / (1.0 + value_b + 0.48 * value_b * value_b + 0.235 * value_b * value_b * value_b)
-	
-	var scaler_a : float = current - target;
-	var scaler_b : float = target;
-	var max_length : float = max_speed * smooth_time
-	
-	scaler_a = clamp(scaler_a, -max_length, max_length);
-	
-	target = current - scaler_a;
-	var scaler_c : float = (current_velocity + value_a * scaler_a) * delta;
-	current_velocity = (current_velocity - value_a * scaler_c) * value_c;
-	var scaler_d : float = target + (scaler_a + scaler_c) * value_c;
-	if ((scaler_b - current) > 0.0 == (scaler_d > scaler_b)):
-		scaler_d = scaler_b
-		current_velocity = (scaler_d - scaler_b) / delta;
-	
-	return {"interpolation":scaler_d, "velocity":current_velocity}
-
-
-static func rotate_around(p_transform : Transform3D, p_point : Vector3, p_axis : Vector3, p_angle : float) -> Transform3D:
-	var vector : Vector3 = p_point + (Quaternion(p_axis, p_angle) * (p_transform.origin - p_point))
-	p_transform.origin = vector
-	
-	return p_transform.rotated(p_axis, p_angle * 0.0174532924)
-
-
-static func base_log(a : float, new_base : float) -> float:
-	return log(a) / log(new_base)
-
-
-static func transform_directon_vector(p_direction : Vector3, p_basis : Basis) -> Vector3:
-	return Vector3(((p_basis.x.x * p_direction.x) + (p_basis.y.x * p_direction.y) + (p_basis.z.x * p_direction.z)), ((p_basis.x.y * p_direction.x) + (p_basis.y.y * p_direction.y) + (p_basis.z.y * p_direction.z)),((p_basis.x.z * p_direction.x) + (p_basis.y.z * p_direction.y) + (p_basis.z.z * p_direction.z)))
-
-
 static func get_interpolated_transform(p_current_transform : Transform3D, p_target_transform : Transform3D, p_origin_interpolation_factor : float, p_rotation_interpolation_factor : float, p_delta : float):
 	var current_origin : Vector3 = p_current_transform.origin
 	var current_rotation : Basis = p_current_transform.basis
@@ -68,22 +29,6 @@ static func get_interpolated_transform(p_current_transform : Transform3D, p_targ
 	return Transform3D(current_rotation, current_origin)
 
 
-static func vec2rad_to_deg(p_vector2 : Vector2) -> Vector2:
-	return Vector2(rad_to_deg(p_vector2.x), rad_to_deg(p_vector2.y))
-
-
-static func vec2deg2rad(p_vector2 : Vector2) -> Vector2:
-	return Vector2(deg_to_rad(p_vector2.x), deg_to_rad(p_vector2.y))
-
-
-static func vec3rad_to_deg(p_vector3 : Vector3) -> Vector3:
-	return Vector3(rad_to_deg(p_vector3.x), rad_to_deg(p_vector3.y), rad_to_deg(p_vector3.z))
-
-
-static func vec3deg2rad(p_vector3 : Vector3) -> Vector3:
-	return Vector3(deg_to_rad(p_vector3.x), deg_to_rad(p_vector3.y), deg_to_rad(p_vector3.z))
-
-
 static func sanitise_float(p_float : float) -> float:
 	var return_float : float = p_float
 	if is_nan(return_float) or is_inf(return_float):
@@ -91,15 +36,6 @@ static func sanitise_float(p_float : float) -> float:
 		
 	return return_float
 
-
-static func sanitise_vec2(p_vec2 : Vector2) -> Vector2:
-	var return_vec2 : Vector2 = p_vec2
-	if is_nan(return_vec2.x) or is_inf(return_vec2.x):
-		return_vec2.x = 0.0
-	if is_nan(return_vec2.y) or is_inf(return_vec2.y):
-		return_vec2.y = 0.0
-		
-	return return_vec2
 
 static func sanitise_vec3(p_vec3 : Vector3) -> Vector3:
 	var return_vec3 : Vector3 = p_vec3
@@ -125,8 +61,3 @@ static func sanitise_quat(p_quat : Quaternion) -> Quaternion:
 		return_quat.w = 1.0
 		
 	return return_quat
-
-
-static func shortest_angle_distance(p_from: float, p_to: float) -> float:
-	var difference: float = fmod(p_to - p_from, PI * 2.0)
-	return fmod(2.0 * difference, PI * 2.0) - difference
