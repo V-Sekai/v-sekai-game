871939bcfd322faddb260465c2275d956bcdb4a7
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2024-03-09 17:37:10 -0800

    Delete fetch.sh and format.sh scripts, update pre-commit-config.yaml with new hooks for copyright-headers.

--- .github/fetch.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
-
-git clone https://github.com/V-Sekai/godot.git $SCRIPTPATH/../../vsk_godot -b groups-4.x
-git clone https://github.com/V-Sekai/godot-vsekai-merge.git $SCRIPTPATH/../../vsk_merge
-git clone https://github.com/V-Sekai/groups-gocd-pipelines.git $SCRIPTPATH/../../vsk_cicd
-cp -rf $SCRIPTPATH/vscode $SCRIPTPATH/../../.vscode
