759cd6c36a4212b919a0d907287d388dc0262ae0
dragonhunt02 <jackhunt43@libero.it>
2025-01-20 01:59:52 +0000

    Remove unused stale Godot XR Tools code

--- addons/godot-xr-tools/objects/interactable_area.gd
+++
@@ -1,9 +0,0 @@
-class_name XRToolsInteractableArea
-extends Area3D
-
-
-signal pointer_pressed(at)
-signal pointer_released(at)
-signal pointer_moved(from, to)
-signal pointer_entered()
-signal pointer_exited()
--- addons/godot-xr-tools/objects/wind_area.gd
+++
@@ -1,8 +0,0 @@
-class_name XRToolsWindArea
-extends Area3D
-
-## Vector (direction and magnitude) of wind in this area
-@export var wind_vector : Vector3 = Vector3.ZERO
-
-## Wind drag factor
-@export var drag : float = 1.0
